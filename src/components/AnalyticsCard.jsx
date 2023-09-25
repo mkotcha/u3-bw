3a8738c229b1f5d2d8b075dbc9e82826530e3637
Emanuele Zollo - NjS Studio <ez.studionjs@gmail.com>
2023-09-25 17:26:08 +0200

    padding and margin adjustments 1

--- src/components/AnalyticsCard.jsx
+++ src/components/AnalyticsCard.jsx
@@ -11,20 +11,14 @@ function AnalyticsCard () {
           <Card.Subtitle> <i class="bi bi-eye-fill"></i> Private to you </Card.Subtitle>   
             <Container>
                 <Row>
-
-                    <Col sm={4}>
-                        
+                    <Col sm={4}> 
                         <Card.Link href="#"><i class="bi bi-people-fill"></i>15 profile views</Card.Link>
                         <Card.Text>Discover who's viewed your profile.</Card.Text>
-                     
                     </Col>
                     <Col sm={4}>
-                        
                             <Card.Link href="#"><i class="bi bi-search"></i> 2 search appearances</Card.Link>
                             <Card.Text>See how often you appear in search results.</Card.Text>
-                      
                     </Col>
-    
                 </Row>
             </Container>
         </Card.Body>
