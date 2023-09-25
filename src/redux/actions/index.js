785f17af72688bf1deea69b440e28391cbc236fc
Godwin <godwinmail6@gmail.com>
2023-09-25 13:42:37 +0200

    committed

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,20 +1,20 @@
-export const SET_PERSONAL_PROFILE = "SET_PERSONAL_PROFILE";
-export const SET_OTHER_PROFILES = "SET_OTHER_PROFILES";
+export const SET_PERSONAL_PROFILE = 'SET_PERSONAL_PROFILE';
+export const SET_OTHER_PROFILES = 'SET_OTHER_PROFILES';
 
-export const setPersonalProfile = personalProfile => ({ type: SET_PERSONAL_PROFILE, payload: personalProfile });
-export const setOtherProfiles = otherProfils => ({ type: SET_OTHER_PROFILES, payload: otherProfils });
+export const setPersonalProfile = (personalProfile) => ({ type: SET_PERSONAL_PROFILE, payload: personalProfile });
+export const setOtherProfiles = (otherProfils) => ({ type: SET_OTHER_PROFILES, payload: otherProfils });
 
-const url = "https://striveschool-api.herokuapp.com/api/profile/";
+const url = 'https://striveschool-api.herokuapp.com/api/profile/';
 const options = {
-  headers: { Authorization: "Bearer " + process.env.REACT_APP_BEARER },
+  headers: { Authorization: 'Bearer ' + process.env.REACT_APP_BEARER },
 };
 
 export const personalFetch = () => {
-  return async dispatch => {
-    console.log("personal fetch");
+  return async (dispatch) => {
+    console.log('personal fetch');
     try {
-      const response = await fetch(url + "me", options);
-
+      const response = await fetch(url + 'me', options);
+      console.log(options);
       if (response.ok) {
         const result = await response.json();
         dispatch(setPersonalProfile(result));
@@ -27,14 +27,14 @@ export const personalFetch = () => {
 };
 
 export const otherProfileFetch = () => {
-  return async dispatch => {
+  return async (dispatch) => {
     let profilesArray = [];
-    console.log("other profile fetch");
+    console.log('other profile fetch');
     try {
       const resp = await fetch(url, {
         headers: {
           Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExM2NlOTM3NTJhODAwMTQ1Njg3NjciLCJpYXQiOjE2OTU2Mjg1MjIsImV4cCI6MTY5NjgzODEyMn0.SrkS56T505QY6pnzO8arD1CibOq1XJ9LxYw0yBOMRm4",
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExM2NlOTM3NTJhODAwMTQ1Njg3NjciLCJpYXQiOjE2OTU2Mjg1MjIsImV4cCI6MTY5NjgzODEyMn0.SrkS56T505QY6pnzO8arD1CibOq1XJ9LxYw0yBOMRm4',
         },
       });
 
