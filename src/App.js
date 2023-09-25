2fe83b309e1373f9ae1db47d16a79d2729cfaa00
flaviodeangelis56 <deangelisflavio@gmail.com>
2023-09-25 11:30:27 +0200

    first-aside-commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
-import Test from "./components/Test";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test />} />
+        <Route path="/" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/AsideProfilePage.jsx
+++ src/components/AsideProfilePage.jsx
@@ -1,4 +1,45 @@
+import { useState } from "react";
+import { Button, Container } from "react-bootstrap";
+
 const AsideProfilePage = () => {
-  return <></>;
+  const [profiles, setProfile] = useState([]);
+
+  return (
+    <>
+      <div className="border-dark-subtle border rounded py-2 px-2 mb-2">
+        <div className="d-flex justify-content-between mb-2">
+          <h5>Profile language</h5>
+          <i class="bi bi-pencil"></i>
+        </div>
+        <div className="d-flex justify-content-between">
+          <Button variant="success" className="rounded-pill bottoniLingueAside">
+            Italiano
+          </Button>
+          <Button variant="light" className="rounded-pill bottoniLingueAside">
+            English
+          </Button>
+        </div>
+        <Container>
+          <hr />
+        </Container>
+        <div className="d-flex justify-content-between mb-2">
+          <h5 className="d-inline">Public profile & URL</h5>
+          <i class="bi bi-pencil"></i>
+        </div>
+        <p>www.linkedin.com/in/flavio-de-angelis-4b50b3293</p>
+      </div>
+      <div className="border-dark-subtle border rounded mb-2">
+        <img
+          src="https://media.licdn.com/media/AAYQAgTPAAgAAQAAAAAAADVuOvKzTF-3RD6j-qFPqhubBQ.png"
+          alt="see who's hiring"
+          className="w-100"
+        />
+      </div>
+      <div className="border-dark-subtle border rounded py-2 px-2 mb-2">
+        <h6 className="mb-0">People you may know</h6>
+        <p>From your school</p>
+      </div>
+    </>
+  );
 };
 export default AsideProfilePage;
