f53901d143fdd5bb0ca3c4b8293b2e50c0d226da
Godwin <godwinmail6@gmail.com>
2023-09-25 16:10:13 +0200

    commit

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,23 +1,23 @@
-export const SET_PERSONAL_PROFILE = "SET_PERSONAL_PROFILE";
-export const SET_OTHER_PROFILES = "SET_OTHER_PROFILES";
-export const SHOW_PROFILE_MODAL = "SHOW_PROFILE_MODAL";
-export const HIDE_PROFILE_MODAL = "HIDE_PROFILE_MODAL";
+export const SET_PERSONAL_PROFILE = 'SET_PERSONAL_PROFILE';
+export const SET_OTHER_PROFILES = 'SET_OTHER_PROFILES';
+export const SHOW_PROFILE_MODAL = 'SHOW_PROFILE_MODAL';
+export const HIDE_PROFILE_MODAL = 'HIDE_PROFILE_MODAL';
 
 export const setPersonalProfile = personalProfile => ({ type: SET_PERSONAL_PROFILE, payload: personalProfile });
 export const setOtherProfiles = otherProfils => ({ type: SET_OTHER_PROFILES, payload: otherProfils });
 export const showProfileModal = () => ({ type: SHOW_PROFILE_MODAL });
 export const hideProfileModal = () => ({ type: HIDE_PROFILE_MODAL });
 
-const url = "https://striveschool-api.herokuapp.com/api/profile/";
+const url = 'https://striveschool-api.herokuapp.com/api/profile/';
 const options = {
-  headers: { Authorization: "Bearer " + process.env.REACT_APP_BEARER },
+  headers: { Authorization: 'Bearer ' + process.env.REACT_APP_BEARER },
 };
 
 export const personalFetch = () => {
   return async dispatch => {
     // console.log("personal fetch");
     try {
-      const response = await fetch(url + "me", options);
+      const response = await fetch(url + 'me', options);
       if (response.ok) {
         const result = await response.json();
         dispatch(setPersonalProfile(result));
@@ -31,12 +31,12 @@ export const personalFetch = () => {
 export const otherProfileFetch = () => {
   return async dispatch => {
     let profilesArray = [];
-    console.log("other profile fetch");
+    console.log('other profile fetch');
     try {
       const resp = await fetch(url, {
         headers: {
           Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExM2NlOTM3NTJhODAwMTQ1Njg3NjciLCJpYXQiOjE2OTU2Mjg1MjIsImV4cCI6MTY5NjgzODEyMn0.SrkS56T505QY6pnzO8arD1CibOq1XJ9LxYw0yBOMRm4",
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExM2NlOTM3NTJhODAwMTQ1Njg3NjciLCJpYXQiOjE2OTU2Mjg1MjIsImV4cCI6MTY5NjgzODEyMn0.SrkS56T505QY6pnzO8arD1CibOq1XJ9LxYw0yBOMRm4',
         },
       });
 
@@ -56,9 +56,9 @@ export const otherProfileFetch = () => {
 export const putPersonalFetch = async profile => {
   const putOptions = {
     ...options,
-    method: "PUT",
+    method: 'PUT',
     body: JSON.stringify(profile),
-    headers: { ...options.headers, "Content-Type": "application/json" },
+    headers: { ...options.headers, 'Content-Type': 'application/json' },
   };
 
   try {
