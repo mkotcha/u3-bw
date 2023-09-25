390cfc0cf2f1fe076f7ac45b2dcb27449daa5100
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-25 13:30:08 +0200

    ..

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -1,20 +1,37 @@
-import { Button, Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const ProfileModal = () => {
+  const me = useSelector(state => state.me);
+
   return (
     <div className="modal show" style={{ display: "block", position: "initial" }}>
       <Modal.Dialog>
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Edit intro</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <p>Modal body text goes here.</p>
+          <p>
+            <small className="text-secondary">* Indicates required</small>
+          </p>
+          <Form id="personal-form">
+            <Form.Group className="mb-3 text-secondary">
+              <Form.Label>First Name*</Form.Label>
+              <Form.Control type="text" placeholder={me.name} required />
+            </Form.Group>
+            <Form.Group className="mb-3 text-secondary">
+              <Form.Label>Last Name*</Form.Label>
+              <Form.Control type="text" placeholder={me.surname} required />
+            </Form.Group>
+          </Form>
         </Modal.Body>
 
         <Modal.Footer>
           <Button variant="secondary">Close</Button>
-          <Button variant="primary">Save changes</Button>
+          <Button type="submit" form="personal-form" variant="primary">
+            Save changes
+          </Button>
         </Modal.Footer>
       </Modal.Dialog>
     </div>
