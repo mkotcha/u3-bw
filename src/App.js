5cdb8c0a8a88e1c42b4601baa36aa4f839d36e6a
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-25 11:34:02 +0200

    me reducer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Test from "./components/Test";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { personalFetch } from "./redux/actions";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log("yo!");
+    dispatch(personalFetch());
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Routes>
