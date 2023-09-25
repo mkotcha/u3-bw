f7832d59c6dc3890e20d794a21f1fdb32b66fed4
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-25 15:09:25 +0200

    modal close

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -1,9 +1,11 @@
 import { Button, Form, Modal } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { putPersonalFetch } from "../redux/actions";
+import { useState } from "react";
 
 const ProfileModal = () => {
   const me = useSelector(state => state.me);
+  const [show, setShow] = useState(true);
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -21,57 +23,60 @@ const ProfileModal = () => {
     putPersonalFetch(profile);
   };
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="modal show" style={{ display: "block", position: "initial" }}>
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit intro</Modal.Title>
-        </Modal.Header>
+    <Modal show={show} size="lg">
+      <Modal.Header closeButton>
+        <Modal.Title>Edit intro</Modal.Title>
+      </Modal.Header>
 
-        <Modal.Body>
-          <p>
-            <small className="text-secondary">* Indicates required</small>
-          </p>
-          <Form id="personal-form" onSubmit={event => handleSubmit(event)}>
-            <Form.Group className="mb-3 text-secondary">
-              <Form.Label>First Name*</Form.Label>
-              <Form.Control id="name" type="text" defaultValue={me.name} required />
-            </Form.Group>
-            <Form.Group className="mb-3 text-secondary">
-              <Form.Label>Last Name*</Form.Label>
-              <Form.Control id="surname" type="text" defaultValue={me.surname} required />
-            </Form.Group>
-            <Form.Group className="mb-3 text-secondary">
-              <Form.Label>email*</Form.Label>
-              <Form.Control id="email" type="email" defaultValue={me.email} required />
-            </Form.Group>
-            <Form.Group className="mb-3 text-secondary">
-              <Form.Label>Bio*</Form.Label>
-              <Form.Control id="bio" type="textarea" defaultValue={me.bio} required />
-            </Form.Group>
-            <Form.Group className="mb-3 text-secondary">
-              <Form.Label>Title*</Form.Label>
-              <Form.Control id="title" type="text" defaultValue={me.title} required />
-            </Form.Group>
-            <Form.Group className="mb-3 text-secondary">
-              <Form.Label>Area*</Form.Label>
-              <Form.Control id="area" type="text" defaultValue={me.area} required />
-            </Form.Group>
-            <Form.Group className="mb-3 text-secondary">
-              <Form.Label>Image url</Form.Label>
-              <Form.Control id="image" type="text" defaultValue={me.image} />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
+      <Modal.Body>
+        <p>
+          <small className="text-secondary">* Indicates required</small>
+        </p>
+        <Form id="personal-form" onSubmit={event => handleSubmit(event)}>
+          <Form.Group className="mb-3 text-secondary">
+            <Form.Label>First Name*</Form.Label>
+            <Form.Control id="name" type="text" defaultValue={me.name} required />
+          </Form.Group>
+          <Form.Group className="mb-3 text-secondary">
+            <Form.Label>Last Name*</Form.Label>
+            <Form.Control id="surname" type="text" defaultValue={me.surname} required />
+          </Form.Group>
+          <Form.Group className="mb-3 text-secondary">
+            <Form.Label>email*</Form.Label>
+            <Form.Control id="email" type="email" defaultValue={me.email} required />
+          </Form.Group>
+          <Form.Group className="mb-3 text-secondary">
+            <Form.Label>Bio*</Form.Label>
+            <Form.Control id="bio" type="textarea" defaultValue={me.bio} required />
+          </Form.Group>
+          <Form.Group className="mb-3 text-secondary">
+            <Form.Label>Title*</Form.Label>
+            <Form.Control id="title" type="text" defaultValue={me.title} required />
+          </Form.Group>
+          <Form.Group className="mb-3 text-secondary">
+            <Form.Label>Area*</Form.Label>
+            <Form.Control id="area" type="text" defaultValue={me.area} required />
+          </Form.Group>
+          <Form.Group className="mb-3 text-secondary">
+            <Form.Label>Image url</Form.Label>
+            <Form.Control id="image" type="text" defaultValue={me.image} />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
 
-        <Modal.Footer>
-          <Button variant="secondary">Close</Button>
-          <Button type="submit" form="personal-form" variant="primary">
-            Save changes
-          </Button>
-        </Modal.Footer>
-      </Modal.Dialog>
-    </div>
+      <Modal.Footer>
+        <Button onClick={handleClose} variant="secondary">
+          Close
+        </Button>
+        <Button type="submit" form="personal-form" variant="primary">
+          Save changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
