e5b74534797a3f1610f1e3d7d2f2f40b0c2c806b
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-26 22:25:48 +0200

    post form data picture ok

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,11 +1,54 @@
-import ExperienceModal from "./ExperienceModal";
-import ProfileModal from "./ProfileModal";
+import { Button, Container, Form } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const Test = () => {
+  const id = useSelector(state => state.me._id);
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("profile", event.target.formFile.files[0]);
+
+    console.log(JSON.stringify(formData));
+
+    const url = "https://striveschool-api.herokuapp.com/api/profile/";
+
+    const options = {
+      body: formData,
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + process.env.REACT_APP_BEARER,
+      },
+    };
+
+    try {
+      const response = await fetch(url + id + "/picture", options);
+
+      if (response.ok) {
+        const result = await response.json();
+        //dispatch(());
+
+        console.log(result);
+      }
+    } catch (error) {
+      console.log("error: " + error);
+    }
+  };
+
   return (
     <>
-      <p>ciao!</p>
-      <ExperienceModal />
+      <Container>
+        <p>ciao!</p>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Select a photo</Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
+          <Form.Group>
+            <Button type="submit">Submit</Button>
+          </Form.Group>
+        </Form>
+      </Container>
     </>
   );
 };
