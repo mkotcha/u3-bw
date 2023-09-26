555e9e8181a2e6a60958e0defd41496f2702f4b6
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-26 10:29:39 +0200

    actions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,9 +1,11 @@
 export const SET_PERSONAL_PROFILE = "SET_PERSONAL_PROFILE";
+export const SET_SELECTED_PROFILE = "SET_SELECTED_PROFILE";
 export const SET_OTHER_PROFILES = "SET_OTHER_PROFILES";
 export const SHOW_PROFILE_MODAL = "SHOW_PROFILE_MODAL";
 export const HIDE_PROFILE_MODAL = "HIDE_PROFILE_MODAL";
 
 export const setPersonalProfile = personalProfile => ({ type: SET_PERSONAL_PROFILE, payload: personalProfile });
+export const setSelectedProfile = selectedProfile => ({ type: SET_SELECTED_PROFILE, payload: selectedProfile });
 export const setOtherProfiles = otherProfils => ({ type: SET_OTHER_PROFILES, payload: otherProfils });
 export const showProfileModal = () => ({ type: SHOW_PROFILE_MODAL });
 export const hideProfileModal = () => ({ type: HIDE_PROFILE_MODAL });
@@ -13,14 +15,18 @@ const options = {
   headers: { Authorization: "Bearer " + process.env.REACT_APP_BEARER },
 };
 
-export const personalFetch = () => {
+export const personalFetch = (id = "me") => {
   return async dispatch => {
     // console.log("personal fetch");
     try {
-      const response = await fetch(url + "me", options);
+      const response = await fetch(url + id, options);
       if (response.ok) {
         const result = await response.json();
-        dispatch(setPersonalProfile(result));
+        if (id === "me") {
+          dispatch(setPersonalProfile(result));
+        } else {
+          dispatch(setSelectedProfile(result));
+        }
       }
     } catch (error) {
       console.log(error);
@@ -30,40 +36,38 @@ export const personalFetch = () => {
 
 export const otherProfileFetch = () => {
   return async dispatch => {
-    let profilesArray = [];
-    console.log("other profile fetch");
     try {
       const resp = await fetch(url, options);
       if (resp.ok) {
-        profilesArray = await resp.json();
+        const profilesArray = await resp.json();
         dispatch(setOtherProfiles(profilesArray));
-        return profilesArray;
+        // return profilesArray;
       }
     } catch (error) {
       console.log(error);
-    } finally {
-      return profilesArray;
     }
   };
 };
 
-export const putPersonalFetch = async profile => {
-  const putOptions = {
-    ...options,
-    method: "PUT",
-    body: JSON.stringify(profile),
-    headers: { ...options.headers, "Content-Type": "application/json" },
-  };
+export const putPersonalFetch = profile => {
+  return async dispatch => {
+    const putOptions = {
+      ...options,
+      method: "PUT",
+      body: JSON.stringify(profile),
+      headers: { ...options.headers, "Content-Type": "application/json" },
+    };
 
-  try {
-    const response = await fetch(url, putOptions);
+    try {
+      const response = await fetch(url, putOptions);
 
-    if (response.ok) {
-      const result = await response.json();
-
-      console.log(result);
+      if (response.ok) {
+        const result = await response.json();
+        dispatch(personalFetch());
+        console.log(result);
+      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
+  };
 };
