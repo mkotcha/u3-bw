6f045a737bc826654a49e90e19e71253615a03f1
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-26 10:29:06 +0200

    topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Button, Col, Form, Image, Offcanvas, Row } from "react-bootstrap";
+import { Button, Col, Form, Image, Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
