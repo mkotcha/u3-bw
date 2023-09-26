59c2ad1b358b796d382f895070e7d34d7fc65ad8
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-26 10:26:55 +0200

    rec 4 u

--- src/components/recommended4u.jsx
+++ src/components/recommended4u.jsx
@@ -1,22 +1,33 @@
-import Card from 'react-bootstrap/Card';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import { Container } from 'react-bootstrap';
+import Card from "react-bootstrap/Card";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import { Container, Row, Col } from "react-bootstrap";
 
-
-function Recommended4u () {
+function Recommended4u() {
   return (
-    <Card className='mt-3'>
+    <Card className="mt-3">
       <Card.Body>
         <Card.Title>Suggested for you</Card.Title>
-        <Card.Subtitle> <i class="bi bi-eye-fill"></i> Private to you </Card.Subtitle>
-        <Card.Subtitle className='mt-4'>Intermediate</Card.Subtitle>
-        <Container>
-        <ProgressBar now={90}/> <span>6/7</span>;
+        <Card.Subtitle>
+          {" "}
+          <i className="bi bi-eye-fill"></i> Private to you{" "}
+        </Card.Subtitle>
+        <Card.Subtitle className="mt-4 mb-3">Intermediate</Card.Subtitle>
+        <Container className="mb-3 align-items-center">
+          <Row>
+            <Col sm={11}>
+              <ProgressBar now={90} />
+            </Col>
+            <Col sm={1}>
+              <Card.Text>6/7</Card.Text>
+            </Col>
+          </Row>
         </Container>
         <Card.Text>
-        Complete 2 steps to achieve 
-           {/* //TODO BLU - hover sottolineato - click si apre un modale */}
-           <Card.Link href="#">All-star</Card.Link>
+          Complete 2 steps to achieve
+          {/* //TODO BLU - hover sottolineato - click si apre un modale */}
+          <Card.Link className="mx-2" href="#">
+            All-star
+          </Card.Link>
         </Card.Text>
       </Card.Body>
     </Card>
