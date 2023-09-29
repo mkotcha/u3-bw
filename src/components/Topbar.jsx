920eab101b009a4c4948203cbab9aef2d2ea7008
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-29 15:29:32 +0200

    topBar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -26,114 +26,114 @@ const Topbar = () => {
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary sticky-top p-0 w-100">
-        <Row className="justify-content-center w-100 mx-auto">
-          <Col lg={7}>
-            <div className="justify-content-around">
-              <Row className="justify-content-between">
-                <Col xs={3} className="align-self-center">
-                  <div className="d-flex">
-                    <Link to="/">
-                      <Image
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/800px-LinkedIn_icon.svg.png"
-                        alt="Logo"
-                        width={40}
-                        height={40}
-                        className="me-2"
-                      />
-                    </Link>
+        <Row className="justify-content-center align-items-center w-100 mx-auto">
+          <Col xs={12} md={12} lg={10} xxl={8} className="p-0">
+            <div className="d-flex w-100">
+              <div className="d-flex align-items-center">
+                <Link to="/">
+                  <Image
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/800px-LinkedIn_icon.svg.png"
+                    alt="Logo"
+                    width={40}
+                    height={40}
+                    className=""
+                  />
+                </Link>
+              </div>
 
-                    <i className="bi bi-search d-lg-none fontSizeIconTopBar"></i>
-                    <Form onSubmit={handleSubmit}>
-                      <Form.Control
-                        type="text"
-                        placeholder="Search"
-                        className="mr-sm-2 d-none d-lg-inline"
-                        onChange={e => setChangeJob(e.target.value)}
-                      />
-                      <Button className="d-none" type="submit">
-                        click
-                      </Button>
-                    </Form>
+              {/* CONTAINER ICONE NAV */}
+
+              <div className="d-flex flex-grow-1 w-100 px-4">
+                <Nav className="w-100">
+                  <div className="d-flex gap-4 justify-content-between w-100">
+                    <div className="nav-link ">
+                      <div className="d-flex flex-column text-center text-reset text-decoration-none">
+                        <i className="bi bi-search d-lg-none fontSizeIconTopBar"></i>
+                        <span className="smalltext text-center d-none d-md-inline d-lg-none">Search</span>
+                      </div>
+                      <div className="d-flex h-100 align-items-center">
+                        <Form onSubmit={handleSubmit} className="d-none d-lg-inline">
+                          <Form.Control
+                            type="text"
+                            placeholder="Search"
+                            className="mr-sm-2 "
+                            onChange={e => setChangeJob(e.target.value)}
+                          />
+                          <Button className="d-none" type="submit">
+                            click
+                          </Button>
+                        </Form>
+                      </div>
+                    </div>
+
+                    <NavLink to={"/"} className="nav-link">
+                      <div className="d-flex flex-column text-center text-reset text-decoration-none">
+                        <i className="bi bi-house-door-fill text-center fontSizeIconTopBar"></i>
+                        <span className="smalltext text-center d-none d-md-inline">Home</span>
+                      </div>
+                    </NavLink>
+                    <NavLink to={"/My Network"} className="nav-link">
+                      <div className="d-flex flex-column text-center text-reset text-decoration-none">
+                        <i className="bi bi-people-fill text-center fontSizeIconTopBar"></i>
+                        <span className="smalltext text-center d-none d-md-inline">My Network</span>
+                      </div>
+                    </NavLink>
+                    <NavLink to={"/Jobs"} className="nav-link">
+                      <div className="d-flex flex-column text-center text-reset text-decoration-none">
+                        <i className="bi bi-briefcase-fill text-center fontSizeIconTopBar"></i>
+                        <span className="smalltext text-center d-none d-md-inline">Jobs</span>
+                      </div>
+                    </NavLink>
+                    <NavLink to={"/Messaging"} className="nav-link">
+                      <div className="d-flex flex-column text-center text-reset text-decoration-none">
+                        <i className="bi bi-chat-left-dots-fill text-center fontSizeIconTopBar"></i>
+                        <span className="smalltext text-center d-none d-md-inline">Messaging</span>
+                      </div>
+                    </NavLink>
+                    <NavLink to={"/Notification"} className="nav-link">
+                      <div className="d-flex flex-column text-center text-reset text-decoration-none">
+                        <i className="bi bi-bell-fill text-center fontSizeIconTopBar"></i>
+                        <span className="smalltext text-center d-none d-md-inline">Notification</span>
+                      </div>
+                    </NavLink>
+
+                    <div className="align-self-center d-flex-inline flex-column align-items-center me-3">
+                      <Link to={"/profilepage/"}>
+                        <img
+                          src={personalProfile.image}
+                          alt="profile"
+                          className="rounded-circle topbar-image-profile"
+                        />
+                      </Link>
+                      <NavDropdown title="Me" id="basic-nav-dropdown" className="d-none d-md-inline">
+                        <h5 className="px-2">Account</h5>
+                        <NavDropdown.Item href="#action/3.4">work </NavDropdown.Item>
+                        <Button variant="primary" className="mx-1">
+                          View profile
+                        </Button>{" "}
+                        <hr></hr>
+                        <h5 className="px-2">Account</h5>
+                        <p className="px-2">Try Premium free</p>
+                        <p className="px-2">Setting and Privacy</p>
+                        <p className="px-2">Help</p>
+                        <p className="px-2">Language</p>
+                        <hr></hr>
+                        <h5 className="px-2">Gestisci</h5>
+                        <p className="px-2">Post an activity</p>
+                        <p className="px-2">Account </p>
+                        <hr></hr>
+                        <p className="px-2">Quit</p>
+                      </NavDropdown>
+                    </div>
+                    <div className="align-self-center border-start d-flex flex-column align-items-center">
+                      <i className="bi bi-grid-3x3-gap-fill ms-3"></i>
+                      <NavDropdown title="Per le aziende" id="basic-nav-dropdown" className="d-none d-md-inline">
+                        <NavDropdown.Item href="#action/3.4">Per le aziende</NavDropdown.Item>
+                      </NavDropdown>
+                    </div>
                   </div>
-                </Col>
-                {/* CONTAINER ICONE NAV */}
-                <Col xs={7} lg={7}>
-                  <Container>
-                    <Navbar id="basic-navbar-nav">
-                      <Nav className="me-auto">
-                        <NavLink to={"/"} className="nav-link">
-                          <div className="d-flex flex-column text-center text-reset text-decoration-none">
-                            <i className="bi bi-house-door-fill text-center fontSizeIconTopBar"></i>
-                            <span className="smalltext text-center d-none d-lg-inline">Home</span>
-                          </div>
-                        </NavLink>
-
-                        <NavLink to={"/My Network"} className="nav-link">
-                          <div className="d-flex flex-column text-center text-reset text-decoration-none">
-                            <i className="bi bi-people-fill text-center fontSizeIconTopBar"></i>
-                            <span className="smalltext text-center d-none d-lg-inline">My Network</span>
-                          </div>
-                        </NavLink>
-
-                        <NavLink to={"/Jobs"} className="nav-link">
-                          <div className="d-flex flex-column text-center text-reset text-decoration-none">
-                            <i className="bi bi-briefcase-fill text-center fontSizeIconTopBar"></i>
-                            <span className="smalltext text-center d-none d-lg-inline">Jobs</span>
-                          </div>
-                        </NavLink>
-                        <NavLink to={"/Messaging"} className="nav-link">
-                          <div className="d-flex flex-column text-center text-reset text-decoration-none">
-                            <i className="bi bi-chat-left-dots-fill text-center fontSizeIconTopBar"></i>
-
-                            <span className="smalltext text-center d-none d-lg-inline">Messaging</span>
-                          </div>
-                        </NavLink>
-                        <NavLink to={"/Notification"} className="nav-link">
-                          <div className="d-flex flex-column text-center text-reset text-decoration-none">
-                            <i className="bi bi-bell-fill text-center fontSizeIconTopBar"></i>
-                            <span className="smalltext text-center d-none d-lg-inline">Notification</span>
-                          </div>
-                        </NavLink>
-
-                        <div className="align-self-center d-flex-inline flex-column align-items-center me-3">
-                          <Link to={"/profilepage/"}>
-                            <img
-                              src={personalProfile.image}
-                              alt="profile"
-                              className="rounded-circle topbar-image-profile"
-                            />
-                          </Link>
-                          <NavDropdown title="Me" id="basic-nav-dropdown" className="d-none d-lg-inline">
-                            <h5 className="px-2">Account</h5>
-                            <NavDropdown.Item href="#action/3.4">work </NavDropdown.Item>
-                            <Button variant="primary" className="mx-1">
-                              View profile
-                            </Button>{" "}
-                            <hr></hr>
-                            <h5 className="px-2">Account</h5>
-                            <p className="px-2">Try Premium free</p>
-                            <p className="px-2">Setting and Privacy</p>
-                            <p className="px-2">Help</p>
-                            <p className="px-2">Language</p>
-                            <hr></hr>
-                            <h5 className="px-2">Gestisci</h5>
-                            <p className="px-2">Post an activity</p>
-                            <p className="px-2">Account </p>
-                            <hr></hr>
-                            <p className="px-2">Quit</p>
-                          </NavDropdown>
-                        </div>
-                        <div className="align-self-center border-start d-flex flex-column align-items-center">
-                          <i className="bi bi-grid-3x3-gap-fill ms-3"></i>
-                          <NavDropdown title="Per le aziende" id="basic-nav-dropdown" className="d-none d-lg-inline">
-                            <NavDropdown.Item href="#action/3.4">Per le aziende</NavDropdown.Item>
-                          </NavDropdown>
-                        </div>
-                      </Nav>
-                    </Navbar>
-                  </Container>
-                </Col>
-              </Row>
+                </Nav>
+              </div>
             </div>
           </Col>
         </Row>
